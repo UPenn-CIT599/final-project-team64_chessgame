c7dc650f4e62022b1670b2c1e0d431a87c72d186
prkapoor-seas@seas.upenn.edu <prkapoor@seas.upenn.edu>
2020-03-30 08:34:33 -0700

    Modified Queen.java

--- src/finalproject/Queen.java
+++ src/finalproject/Queen.java
@@ -2,8 +2,9 @@
 /**
  * The Queen class is a concrete class that extends abstract class ChessPiece.
  * It constructs and sets initial positions for the queen of both teams.
- * 
+ *
  * @author Team 64 - JMG
+ * Moves to be checked for: regular capture move
  */
 public class Queen extends ChessPiece {
 

ddf38e88393594a4d5e2035067fb24dc8e53b755
JMG <jmguerin@seas.upenn.edu>
2020-04-04 14:53:04 -0800

    added comments JMG

--- src/finalproject/Runner.java
+++ src/finalproject/Runner.java
@@ -1,7 +1,13 @@
 package finalproject;
 
+/**
+ * The Runner class comprises of the method that launches the game.
+ */
 public class Runner {
     
+/**
+ * This method launches the game.
+ */
     public static void main(String[] args) {
         Board board = new Board();
         board.initialize();

e47698d7949efbbf42be2ed32707c41355b0c4fa
JMG <jmguerin@seas.upenn.edu>
2020-04-04 15:34:34 -0800

    Update ChessPieceTest.java

--- src/finalproject/tests/ChessPieceTest.java
+++ src/finalproject/tests/ChessPieceTest.java
@@ -3,6 +3,9 @@ package finalproject.tests;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
+/** 
+ * This class is the test class for its counterpart class in the program
+ */
 class ChessPieceTest {
 
     @Test

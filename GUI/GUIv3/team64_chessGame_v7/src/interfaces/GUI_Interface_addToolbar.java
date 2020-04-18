3fb65f9307f7a3f79ae3460b5fe9241c5647aee9
JMG <jmguerin@seas.upenn.edu>
2020-04-17 17:30:58 -0800

    Add files via upload

---
+++ GUI/GUIv3/team64_chessGame_v7/src/interfaces/GUI_Interface_addToolbar.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface GUI_Interface_addToolbar {
+	public void add(Object object);
+}
---
+++ GUI/GUIv3/team64_chessGame_v7/src/tests/ChessPieceTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/** 
+ * This class is the test class for its counterpart class in the program
+ */
+class ChessPieceTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}

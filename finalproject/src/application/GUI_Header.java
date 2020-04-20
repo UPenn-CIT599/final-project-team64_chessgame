c591499da7c48f8a05c3bf2aaf51b1c0241cf927
JMG <jmguerin@seas.upenn.edu>
2020-04-20 14:51:06 -0800

    Update GUI_Header.java

--- finalproject/src/application/GUI_Header.java
+++ finalproject/src/application/GUI_Header.java
@@ -17,31 +17,45 @@ public class GUI_Header extends JPanel {
         //still need to add more functionality, player turn, instructions, etc.
 
         notificationPanel = new JPanel();
-        notificationPanel.setAlignmentY(Component.TOP_ALIGNMENT);
-        notificationPanel.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, null, null, null));
-        notificationPanel.setBounds(new Rectangle(50, 50, 50, 50));
-        notificationPanel.setMinimumSize(new Dimension(250, 250));
-        notificationPanel.setMaximumSize(new Dimension(1000, 1000));
-        notificationPanel.setPreferredSize(new Dimension(500, 200));
+//        notificationPanel.setAlignmentY(Component.TOP_ALIGNMENT);
+//        notificationPanel.setLocation(100, 50);
+//        
+//        notificationPanel.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, null, null, null));
+//        notificationPanel.setPreferredSize(new Dimension(500, 200));
+        
         textInNotificationBar = new JTextArea();
+        textInNotificationBar.setEditable(false);
         textInNotificationBar.setWrapStyleWord(true);
-        textInNotificationBar.setBounds(43, 52, 714, 21);
+        textInNotificationBar.setBounds(1, 1, 500, 200);
+        textInNotificationBar.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, null, null, null));
 
-        setLayout(new FlowLayout(FlowLayout.CENTER));
+//        setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        notificationPanel.setBackground(Color.WHITE);
-        notificationPanel.setBounds(200, 75, 600, 200);
-        notificationPanel.setLayout(null);
+//        notificationPanel.setBackground(Color.WHITE);
+//        notificationPanel.setLayout(null);
 
         textInNotificationBar.setFont(new Font("Arial", Font.BOLD, 14));
-        textInNotificationBar.setLineWrap(false);
+        textInNotificationBar.setLineWrap(true);
+        textInNotificationBar.setWrapStyleWord(true);;
         textInNotificationBar.setBackground(Color.WHITE);
-        textInNotificationBar.setText("NOTIFICATION BAR - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST");
+//        textInNotificationBar.setText("NOTIFICATION BAR - TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST");
 
-        notificationPanel.add(textInNotificationBar);
+//        notificationPanel.add(textInNotificationBar);
         add(notificationPanel);
-
-
+        add(textInNotificationBar);
+        //place holder for action event message handling in info / status bar
+        infoMsgWhiteTeamTurn();
+        infoMsgBlackTeamTurn();
     }
 
+    public void infoMsgWhiteTeamTurn() {
+        textInNotificationBar.append("It is white team's turn! \n");
+//        textInNotificationBar.setText("It is white team's turn!"); //alternate msg
+    }
+    
+    public void infoMsgBlackTeamTurn() {
+        textInNotificationBar.append("It is black team's turn! \n");
+//        textInNotificationBar.setText("It is black team's turn!"); //alternate msg
+    }
+    
 }
